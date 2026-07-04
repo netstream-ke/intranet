480fc30feda15ea2e44c584cf92dddabc6ca87d8
netsream-ke <leonardkibor92@GMAIL.com>
2026-07-04 11:05:01 +0300

    Fix test issues

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -1,6 +1,11 @@
 class UserNotification < ApplicationRecord
   belongs_to :user
-  belongs_to :notifiable, polymorphic: true, optional: true
+  belongs_to :actor, class_name: "User", foreign_key: :actor_id, optional: true
+  belongs_to :notifiable, polymorphic: true
 
   scope :unread, -> { where(read: false) }
+
+  def chat_room
+    notifiable if notifiable.is_a?(ChatRoom)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,19 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Load all fixtures
+  fixtures :all
+end
+
 class ActionDispatch::IntegrationTest
   def sign_in_as(user)
     post login_url, params: {
       email: user.email,
-      password: "password"
+      password: "Kar_elin@12034"
     }
   end
 end
