e1e7459d11f57a3751b37b8771e2a3a410cd2bfa
netsream-ke <leonardkibor92@GMAIL.com>
2026-07-06 15:52:40 +0300

    Configure production for Render deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,9 @@ config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
-  user_name: "leonardkibor92@gmail.com",
-  password: "password",
-  authentication: "plain",
+  user_name: ENV["SMTP_USERNAME"],
+  password: ENV["SMTP_PASSWORD"],
+  authentication: :plain,
   enable_starttls_auto: true
 }
 
